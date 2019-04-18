e61d25c6ef6ee77bd4c6cefc433470edb6a62d9e
Egidijus Gecius <>
2019-04-18 14:19:47 +0100

    show downloaded image, to show that the app communicates with the internet

--- app/src/main/java/com/egecius/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/egecius/firebasedemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.egecius.firebasedemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val fireBaseIcon = "https://firebase.google.com/_static/images/firebase/touchicon-180.png"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Picasso.get().load(fireBaseIcon).into(imageView)
     }
 }
