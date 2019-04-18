fa6962d361aa8f313a8d7539fbc4af0a595af30e
Egidijus Gecius <>
2019-04-18 14:46:58 +0100

    print Firebase token

--- app/src/main/java/com/egecius/firebasedemo/MainActivity.kt
+++ app/src/main/java/com/egecius/firebasedemo/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.egecius.firebasedemo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,5 +18,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Picasso.get().load(fireBaseIcon).into(imageView)
+
+        printFirebaseToken()
+    }
+
+    private fun printFirebaseToken() {
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("Eg:", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                Log.i("Eg:MainActivity:36", "demoFirebase token $token")
+
+                // Log and toast
+//                val msg = getString(R.string.msg_token_fmt, token)
+//                Log.d(TAG, msg)
+                Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+            })
     }
 }
