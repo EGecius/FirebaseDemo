10d6c07a9e023af87bcfe96989fe3fa8387f2c83
Egidijus Gecius <>
2019-04-23 12:19:01 +0100

    print message received

--- app/src/main/java/com/egecius/firebasedemo/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/egecius/firebasedemo/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package com.egecius.firebasedemo
 import android.annotation.SuppressLint
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -11,6 +12,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onNewToken(token)
 
         Log.i("Eg:MyFirebaseMessagingService:10", "onNewToken token $token")
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+
+        val message = remoteMessage?.data
+        Log.i("Eg:MyFirebaseMessagingService:23", "onMessageReceived data $message")
+
     }
 
 }
